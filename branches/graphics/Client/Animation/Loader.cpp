139299cd9effbb2f65203cb84b03131926afd0f9
jlewis604 <jlewis604@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-02-09 21:04:24 +0000

    adding animation V1

---
+++ branches/graphics/Client/Animation/Loader.cpp
@@ -0,0 +1,38 @@
+#include "Loader.h"
+using namespace std;
+/*----------------------------------------------------------------------------------------------
+-- FUNCTION NAME: Loader
+--
+-- PURPOSE: constructor.
+--
+-- DESIGNER: Jordan 
+--
+-- PROGRAMMER: Jordan
+--
+-- DATE: Jan. 31st, 2010
+--
+-- REVISIONS: None
+-----------------------------------------------------------------------------------------------*/
+Loader::Loader()
+{
+	/* do nothing yet */
+}
+/*----------------------------------------------------------------------------------------------
+-- FUNCTION NAME: getAnimation
+--
+-- PURPOSE: returns animation object based on type request.
+--
+-- DESIGNER: Jordan 
+--
+-- PROGRAMMER: Jordan
+--
+-- DATE: Jan. 31st, 2010
+--
+-- REVISIONS: None
+-----------------------------------------------------------------------------------------------*/
+Animation Loader::getAnimation(AnimationType type)
+{
+	/* will be used to build animations, this call will be the starting sequence */
+}
+
+
