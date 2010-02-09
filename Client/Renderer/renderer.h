6be3b5f6dc3fc2e19fe7f33f03df53042df08180
cryolithic <cryolithic@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-02-09 21:58:42 +0000

    fial

---
+++ Client/Renderer/renderer.h
@@ -0,0 +1,32 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <QGLWidget>
+#include <QGLFramebufferObject>
+#include <QGLFramebufferObjectFormat>
+
+    class Renderer : public QGLWidget
+{
+private:
+    QPoint anchor;
+    float scale;
+    float rot_x, rot_y, rot_z;
+    GLuint tile_list;
+    GLfloat *wave;
+
+    QGLFramebufferObject *render_fbo;
+    QGLFramebufferObject *texture_fbo;
+
+public:
+    Renderer(QWidget *parent);
+    void Initialize();
+    void Render();
+//    void paintEvent(QPaintEvent *);
+
+private:
+    void resizeGL(int w, int h);
+    void saveGLState();
+    void restoreGLState();
+};
+
+#endif // RENDERER_H
