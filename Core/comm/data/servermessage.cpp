9871c4a14d9c89b1d7d32d8c97b0322934f92ff2
kythyx <kythyx@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-02-09 21:21:14 +0000

    Merged Comm

---
+++ Core/comm/data/servermessage.cpp
@@ -0,0 +1,5 @@
+#include "servermessage.h"
+
+ServerMessage::ServerMessage()
+{
+}
---
+++ Core/comm/socketwrapper.cpp
@@ -0,0 +1,77 @@
+#include "socketwrapper.h"
+
+using namespace std;
+
+int SocketWrapper::Socket(int family, int type, int protocol)
+{
+    int n;
+
+    if ( (n = socket(family, type, protocol)) < 0)
+        cerr << "socket error" << endl;
+
+    return (n);
+}
+
+void SocketWrapper::Bind(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+    if (bind(fd, sa, salen) < 0)
+        cerr << "bind error" << endl;
+}
+
+int SocketWrapper::Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+    int n;
+
+    while (1)
+    {
+        if ( (n = accept(fd, sa, salenptr)) < 0)
+        {
+            if (errno == ECONNABORTED)
+                continue;
+            cerr << "accept error" << endl;
+            break;
+        }
+    }
+    return(n);
+}
+
+void SocketWrapper::Connect(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+    if (connect(fd, sa, salen) < 0)
+        cerr << "connect error" << endl;
+}
+
+void SocketWrapper::SocketWrapper::Listen(int fd, int backlog)
+{
+    char *ptr;
+
+    /* can override 2nd argument with environment variable */
+    if ( (ptr = getenv("LISTENQ")) != NULL)
+        backlog = atoi(ptr);
+
+    if (listen(fd, backlog) < 0)
+        cerr << "listen error" << endl;
+}
+
+void SocketWrapper::Write(int fd, const void *vptr, size_t n)
+{
+    size_t nleft;
+    ssize_t nwritten;
+    const char *ptr;
+
+    ptr = (char*) vptr;
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ( (nwritten = write(fd, ptr, nleft)) <= 0)
+        {
+            if (nwritten < 0 && errno == EINTR)
+                nwritten = 0; /* and call write() again */
+            else
+                cerr << "write error" << endl; /* error */
+        }
+
+        nleft -= nwritten;
+        ptr   += nwritten;
+    }
+}
