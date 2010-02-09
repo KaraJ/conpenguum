c33a1d1fe448f06dd7d377e151df151e4be89dd3
kythyx <kythyx@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-02-09 22:09:17 +0000

    Stuff builds now

--- Core/comm/data/message.cpp
+++ Core/comm/data/message.cpp
@@ -1,6 +1,2 @@
 #include "message.h"
 
-Message::Message(int _ClientID, std::string _text)
-    : ClientID(_ClientID), text(_text)
-{
-}
--- Core/comm/data/message.h
+++ Core/comm/data/message.h
@@ -6,12 +6,12 @@
 class Message
 {
 private:
-    std::string text;
     int ClientID;
+    std::string text;
 
 public:
     Message(int, std::string);
-    virtual unsigned char[] Serialize() = 0;
+    //virtual unsigned char[] Serialize() = 0;
 };
 
 #endif // MESSAGE_H
