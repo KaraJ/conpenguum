ba2120c5b9c40117e5945bc36d495b72476eee88
daraiusdastoor <daraiusdastoor@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-02-21 09:47:36 +0000

    adding all project files for mainmenu (gui)

---
+++ Client/GUI/Project(for testing)/splashitem.h
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------
+--  SOURCE FILE: splashitem.h
+--
+--  PROGRAM:
+--
+--
+--  FUNCTIONS:
+--
+--
+--  DATE: Feb 1, 2010
+--
+--  DESIGNER: Nokia
+--
+--  PROGRAMMER REVISIONS: Daraius Dastoor
+--
+--  NOTES:
+--
+----------------------------------------------------------------------------*/
+
+#include <QtCore/qobject.h>
+#include <QtCore/qtimeline.h>
+#include <QtGui/qgraphicswidget.h>
+
+class SplashItem : public QGraphicsWidget
+{
+    Q_OBJECT
+public:
+    SplashItem(QGraphicsItem *parent = 0);
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+protected:
+    void keyPressEvent(QKeyEvent *event);
+
+private Q_SLOTS:
+    void setValue(qreal value);
+
+private:
+    QTimeLine *timeLine;
+    QString text;
+    qreal opacity;
+};
