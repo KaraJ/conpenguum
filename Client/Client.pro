5189c020f6c7414db14b1a508f6e9e54af3bc4d1
cryolithic <cryolithic@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-02-23 01:04:01 +0000

    update

--- Client/Client.pro
+++ Client/Client.pro
@@ -1,6 +1,8 @@
 SOURCES += Animation/Loader.cpp \
     Animation/Image.cpp \
-    Animation/Animation.cpp
+    Animation/Animation.cpp \
+    Renderer/renderer.cpp
 HEADERS += Animation/Loader.h \
     Animation/Image.h \
-    Animation/Animation.h
+    Animation/Animation.h \
+    Renderer/renderer.h
--- Client/Renderer/renderer.cpp
+++ Client/Renderer/renderer.cpp
@@ -2,9 +2,16 @@
 #include "rendererDefines.h"
 #include <QtGlobal>
 #include <QPainter>
+#include <QtGui>
+#include <QGLWidget>
+#include <QtOpenGL>
+
+#define checkImageWidth 64
+#define checkImageHeight 64
+static GLuint texName;
 
 Renderer::Renderer(QWidget *parent) : QGLWidget(QGLFormat(QGL::SampleBuffers|QGL::AlphaChannel), parent)
-{        
+{
 }
 
 /*void Renderer::paintEvent(QPaintEvent *)
@@ -15,40 +22,73 @@ Renderer::Renderer(QWidget *parent) : QGLWidget(QGLFormat(QGL::SampleBuffers|QGL
 
 void Renderer::Initialize()
 {
-    //set GL flags
-    //glEnable(GL_DEPTH_TEST);
-    //glEnable(GL_CULL_FACE);
-    glEnable(GL_TEXTURE_2D);
-    glEnable(GL_SMOOTH);
-    qglClearColor(Qt::black);
-    glClearDepth(1.0f);
+    makeCurrent();
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
     glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-    //Set the clear color
+    texName = bindTexture(QPixmap(QString(":/textures/bg07.bmp"),"BMP"), GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texName);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+}
+
+void Renderer::BuildRenderList()
+{
+    //iterate over the map
+    //I imagine, if we use an array for the map (fast) we would have something like
+    //for(int i = 0; i < GameMap::GetInstance()->tileCount; i++);
+    //in fact, we should probably narrow that loop down based on our viewport...
+    //int x,y,k=0;
+    //GetViewPortTopLeft(x,y);
+    //for(int i = x/tileWidth; i<viewPortWidth/tileWidth; i++)
+    //  for(int j = y/tileheight; j .......etc)
+    //  {    renderList[k] = gameMap[i][j]; k++; }
+
+    //iterate over the gamestate object for items within our viewport.
+    //for(iterator::begin .....)
+    // if(item is in viewport)
+    //   renderlist[k] = .....
 }
 
 void Renderer::resizeGL(int w, int h)
 {
-    int side = qMin(w, h);
-    glViewport((w - side) / 2, (h - side) / 2, side, side);
+    glViewport(0,0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 30.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0,0.0,-3.6);
+    gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble) h );
 }
 
 void Renderer::Render()
 {
-    QImage tex("../textures/bg01.bmp");
-    glBindTexture(GL_TEXTURE_2D, bindTexture(tex,GL_TEXTURE_2D));
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1,1,1);
-    glOrtho(0,1,0,1,-1,1);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.25,0.25,0.0);
-        glVertex3f(0.75,0.25,0.0);
-        glVertex3f(0.75,0.75,0.0);
-        glVertex3f(0.25,0.75,0.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_TEXTURE_2D);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    glBindTexture(GL_TEXTURE_2D, texName);
+    glBegin(GL_QUADS);
+    //for(int i = 0; i < renderListSize; i++)
+    //{
+    //  RenderObject rob = renderList[i];
+    //  glTexCoord2f = (rob.xOffset, rob.yOffset); glVertex3f(rob.x-rob.width/2, rob.y-rob.height/2);
+    //  glTexCoord2f = (rob.xOffset, rob.yOffset); glVertex3f(rob.x-rob.width/2, rob.y+rob.height/2);
+    //  glTexCoord2f = (rob.xOffset, rob.yOffset); glVertex3f(rob.x+rob.width/2, rob.y+rob.height/2);
+    //  glTexCoord2f = (rob.xOffset, rob.yOffset); glVertex3f(rob.x+rob.width/2, rob.y-rob.height/2);
+    //}
+    glTexCoord2f(0.0,0.0); glVertex3f(x, x, 0.0);
+    glTexCoord2f(0.0,1.0); glVertex3f(x, y, 0.0);
+    glTexCoord2f(1.0,1.0); glVertex3f(y, y, 0.0);
+    glTexCoord2f(1.0,0.0); glVertex3f(y, x, 0.0);
     glEnd();
     glFlush();
+    glDisable(GL_TEXTURE_2D);
+
     //TODO: This is the text rendering part, this should be moved out to it's own function
     /*QPainter p(this); // used for text overlay
     saveGLState();
@@ -63,6 +103,7 @@ void Renderer::Render()
     QFontMetrics fm(p.font());
     p.drawText(width()/2 - fm.width(str1)/2, 20, str1);
     p.drawText(width()/2 - fm.width(str2)/2, 20 + fm.lineSpacing(), str2);*/
+    updateGL();
 }
 
 
