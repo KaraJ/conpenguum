1646c9eeaca24deba83eb4bf9dfc985bc343de71
cryolithic <cryolithic@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-02 22:22:49 +0000

    checking in merged

--- Client/Renderer/renderer.h
+++ Client/Renderer/renderer.h
@@ -2,36 +2,31 @@
 #define RENDERER_H
 
 #include <QGLWidget>
+#include <QGLFramebufferObject>
+//#include <QGLFramebufferObjectFormat>
 
-struct RenderObject
-{
-    GLuint texture, xLocation, yLocation;
-    GLfloat textureXOffset, textureYOffset;
-    GLshort width, height;
-};
-
-class Renderer : public QGLWidget
+    class Renderer : public QGLWidget
 {
 private:
-    RenderObject RenderList[4096];
     QPoint anchor;
     float scale;
     float rot_x, rot_y, rot_z;
     GLuint tile_list;
     GLfloat *wave;
 
+    QGLFramebufferObject *render_fbo;
+    QGLFramebufferObject *texture_fbo;
+
 public:
     Renderer(QWidget *parent);
     void Initialize();
     void Render();
-    int x,y;
 //    void paintEvent(QPaintEvent *);
 
 private:
     void resizeGL(int w, int h);
     void saveGLState();
     void restoreGLState();
-    void BuildRenderList();
 };
 
 #endif // RENDERER_H
