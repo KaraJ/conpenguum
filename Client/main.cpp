c2ce41c01414cdaf3a9b51e32e2e89be3f6f780f
walrick.box <walrick.box@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-09 18:47:10 +0000

    Reintegrated branches/Client into trunk/Client

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,18 +1,13 @@
-#include "main.h"
+#include "BaseWindow.h"
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    QResource::registerResource("GUI/menuitems.rcc");
-
-    // insert main gfx window initilization here, which extends from BaseWindow 
-    
-    //GameWindow mainWin = new GameWindow();
     BaseWindow mainWin;
-    
+
     mainWin.setWindowTitle("Game Window");
-    
     mainWin.show();
+
     return app.exec();
 }
 
