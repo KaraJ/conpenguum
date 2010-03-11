2db355f44932671f2bfaef22727e951ed82d59f8
darwinsurvivor <darwinsurvivor@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-11 00:09:24 +0000

    added support for selectable size updated to new version of xml format

--- branches/tools/Tools/MapGenerator/gui.py
+++ branches/tools/Tools/MapGenerator/gui.py
@@ -2,6 +2,9 @@ import sys
 from PyQt4 import QtGui, QtCore
 import random
 
+width = int(sys.argv[1])
+height = int(sys.argv[2])
+
 physics_types = [
     {
         'name': 'Space',
@@ -14,13 +17,6 @@ physics_types = [
         'args': {
             'hit': 'bounce'
         }
-    },
-    {
-        'name': 'mine',
-        'description': 'Instant Death',
-        'args': {
-            'hit': 'die'
-        }
     }
 ]
 graphics_types = [
@@ -30,38 +26,53 @@ graphics_types = [
         'args': None
     },
     {
-        'name': 'wall',
+        'name': 'straight',
         'description': 'You will bounce off this',
         'args': {
-            'type': 'wall',
             'src': 'global',
             'filename': 'map_textures.bmp',
-            'h': '0',
-            'v': '0'
+            'blh': '0/19',
+            'blv': '0/10',
+            'trh': '1/19',
+            'trv': '1/10'
         }
     },
     {
-        'name': 'mine',
-        'description': 'Instant Death',
-        'args':  {
-            'type': 'mine',
+        'name': 'corner',
+        'description': 'You will bounce off this',
+        'args': {
             'src': 'global',
             'filename': 'map_textures.bmp',
-            'h': '1',
-            'v': '0'
+            'blh': '1/19',
+            'blv': '0/10',
+            'trh': '2/19',
+            'trv': '1/10'
         }
-    }
+    },
+    {
+        'name': 'intersection',
+        'description': 'You will bounce off this',
+        'args': {
+            'src': 'global',
+            'filename': 'map_textures.bmp',
+            'blh': '2/19',
+            'blv': '0/10',
+            'trh': '3/19',
+            'trv': '1/10'
+        }
+    },
 ]
 
 
 class Tile(QtGui.QLabel):
-    def __init__(self, x, y, physics=None, graphics=None, parent=None):
+    def __init__(self, x, y, physics=None, graphics=None, rotation=None, parent=None):
         super(QtGui.QLabel, self).__init__(parent)
         self.selected = False
         self.x = x
         self.y = y
         self.physics = physics
         self.graphics = graphics
+        self.rotation = rotation
         self.update()
 
     def mousePressEvent(self, event):
@@ -95,6 +106,7 @@ class Tile(QtGui.QLabel):
         if self.graphics == 0:
             return ''
         args = graphics_types[self.graphics]['args']
+        args['rotation'] = self.rotation
         return "<graphics %s />" % ' '.join("%s='%s'" % (key, args[key]) for key in args.keys())
 
 
@@ -106,14 +118,21 @@ class MapWidget(QtGui.QWidget):
         self.width = width
         self.currentPhysics = 0
         self.currentGraphics = 0
+        self.currentGraphicsRotation = 0
 
         self.grid = QtGui.QGridLayout(self)
         self.grid.setSpacing(0)
 
         self.map = [[Tile(x, y) for x in range(width)] for y in range(height)]
-        for y in range(len(self.map)):
-            for x in range(len(self.map[y])):
-                self.grid.addWidget(self.map[x][y], x, y)
+        print width
+        print self.width
+        print len(self.map[0])
+        print height
+        print self.height
+        print len(self.map)
+        for y in range(height):
+            for x in range(width):
+                self.grid.addWidget(self.map[y][x], y, x)
 
     def reset(self):
         for row in self.map:
@@ -134,15 +153,20 @@ class MapWidget(QtGui.QWidget):
     def setCurrentGraphics(self, graphics):
         self.currentGraphics = graphics
 
+    def setCurrentGraphicsRotation(self, rotation):
+        self.currentGraphicsRotation = rotation
+
     def apply(self):
         # get settings
         physics = self.currentPhysics
         graphics = self.currentGraphics
+        rotation = self.currentGraphicsRotation
         for row in self.map:
             for tile in row:
                 if tile.selected:
                     tile.physics = physics
                     tile.graphics = graphics
+                    tile.rotation  = rotation
         self.clear()
 
     def indent(self, level, spacer='    '):
@@ -180,25 +204,39 @@ class Window(QtGui.QMainWindow):
         self.statusBar().showMessage('Welcome')
 
         ### Contents
-        map = MapWidget(25, 25)
+        map = MapWidget(width, height)
         mapBoxWidget = QtGui.QWidget()
 
+        self.physics_label = QtGui.QLabel("Physics:");
         self.physics_selector = QtGui.QComboBox()
         for type in physics_types:
             self.physics_selector.addItem(type['name'])
         self.physics_selector.show()
+        self.physics_box =QtGui.QHBoxLayout()
+        self.physics_box.addWidget(self.physics_label)
+        self.physics_box.addWidget(self.physics_selector)
         self.connect(self.physics_selector, QtCore.SIGNAL("activated(int)"), map.setCurrentPhysics)
 
+        self.graphics_label = QtGui.QLabel("Graphics")
         self.graphics_selector = QtGui.QComboBox()
         for type in graphics_types:
             self.graphics_selector.addItem(type['name'])
         self.graphics_selector.show()
+        self.graphics_rotate_selector = QtGui.QComboBox()
+        for rot in range(4):
+            self.graphics_rotate_selector.addItem("%d degrees" % (rot*90))
+        self.graphics_rotate_selector.show()
+        self.graphics_box = QtGui.QHBoxLayout()
+        self.graphics_box.addWidget(self.graphics_label)
+        self.graphics_box.addWidget(self.graphics_selector)
+        self.graphics_box.addWidget(self.graphics_rotate_selector)
         self.connect(self.graphics_selector, QtCore.SIGNAL("activated(int)"), map.setCurrentGraphics)
+        self.connect(self.graphics_rotate_selector, QtCore.SIGNAL("activated(int)"), map.setCurrentGraphicsRotation)
 
         mapBox = QtGui.QVBoxLayout(mapBoxWidget)
         mapBox.addWidget(map)
-        mapBox.addWidget(self.physics_selector)
-        mapBox.addWidget(self.graphics_selector)
+        mapBox.addLayout(self.physics_box)
+        mapBox.addLayout(self.graphics_box)
         mapBox.addStretch()
 
         hbox = QtGui.QHBoxLayout()
@@ -239,13 +277,6 @@ class Window(QtGui.QMainWindow):
         self.toolbar.addAction(self.quitButton)
 
         self.show()
-
-    def closeEvent(self, event):
-        reply = QtGui.QMessageBox.question(self, 'Message', "Are you sure you want to quit?", QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
-        if reply == QtGui.QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
 
 
 app = QtGui.QApplication(sys.argv)
