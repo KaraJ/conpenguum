ba51581a05f72200883ae5110fada3b6c80ec6db
walrick.box <walrick.box@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-15 05:57:19 +0000

    Merged trunk into engine's branch

--- branches/engine/Client/Animation/Animation.h
+++ branches/engine/Client/Animation/Animation.h
@@ -1,48 +1,166 @@
 #ifndef ANIMATION_H
 #define ANIMATION_H
-#include <string>
 #include <vector>
 #include "Image.h"
-#define NUMBER_OF_ANIMATIONS 5 /*referes to AnimationType definition below*/
-enum AnimationType
-{
-	THRUST, BANK_LEFT, BANK_RIGHT, HIT, FIRE
-};
-/*----------------------------------------------------------------------------------------------
--- CLASSNAME: Animation
+#include <map>
+/*------------------------------------------------------------------------
+-- CLASS NAME: Animation
 --
--- PURPOSE: used to retain vector of animation snap shots. Also retains related speed of animation
---          as well as type.
+-- CLASS PURPOSE: Copy constructor for loading into map.
 --
--- DESIGNER: Jordan
 --
--- PROGRAMMER: Jordan
+-- DESIGNER: Jordan Lewis
 --
--- DATE: Jan. 31st, 2010
+-- PROGRAMMER: Jordan Lewis
 --
--- REVISIONS: None
------------------------------------------------------------------------------------------------*/
+-- DATE: March. 2nd, 2010
+-------------------------------------------------------------------------*/
 class Animation
 {
-	public:
-		Animation();/*implemented in .cpp file*/
-		~Animation(){}/*destructor*/
-		int getXOffSet(){return xOffSet_;}
-		int getYOffSet(){return yOffSet_;}
-		std::vector<Image> buildAnimationVector(AnimationType type);
-		std::string loadAnimationImages(AnimationType type); /* will have to return some sort of image type */
-		std::string getFileLocation(){return fileLocation_;}
-		void setXOffSet(int xOffSet); /*implemented in .cpp file*/
-		void setYOffSet(int yOffSet); /*implemented in .cpp file*/
-		void setFileLocation(std::string location); /*implemented in .cpp file*/
+public:
+    int numFrames_, imagesWide_, imagesTall_;
+    Animation()
+    {
+        numFrames_ = 0;
+        imagesWide_ = 0;
+        imagesTall_ = 0;
+    }
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: setNumFrames
+    --
+    -- FUNCTION PURPOSE: sets the number of frame values.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    void setNumFrames(int number){numFrames_ = number;}
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: setImagesWide
+    --
+    -- FUNCTION PURPOSE: sets images wide on texture.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    void setImagesWide(int imgcount){imagesWide_ = imgcount;}
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: setImagesTall
+    --
+    -- FUNCTION PURPOSE: sets image count height on texture.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    void setImagesTall(int imgcount){imagesTall_ = imgcount;}
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: setAnimationImages
+    --
+    -- FUNCTION PURPOSE: sets animation vector.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    void setAnimationImages(std::vector<Image> images){images_ = images;}
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: getNumFrames
+    --
+    -- FUNCTION PURPOSE: returns number of frames
+    --
+    -- RETURN VALUE: Number of frames.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    int getNumFrames(){return numFrames_;}
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: getImagesWide
+    --
+    -- FUNCTION PURPOSE: returns image wide count.
+    --
+    -- RETURN VALUE: images wide.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    int getImagesWide(){return imagesWide_;}
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: getImagesTall
+    --
+    -- FUNCTION PURPOSE: returns image wide count.
+    --
+    -- RETURN VALUE: images tall.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    int getImagesTall(){return imagesTall_;}
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: getAnimationImages
+    --
+    -- FUNCTION PURPOSE: animation image vector.
+    --
+    -- RETURN VALUE: animation image vector.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    std::vector<Image> getAnimationImages(){return images_;}
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: Animation()
+    --
+    -- FUNCTION PURPOSE: Copy constructor for loading into map.
+    --
+    -- RETURN VALUE: Nothing.
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    Animation(const Animation& a);
+    /*------------------------------------------------------------------------
+    -- FUNCTION NAME: getAnimationMap()
+    --
+    -- FUNCTION PURPOSE: Static method to generate animation map.
+    --
+    -- RETURN VALUE: Map of animations (map<int, Animation> where int is the enum
+    --               for animation type found in AnimationEnum)
+    --
+    -- DESIGNER: Jordan Lewis
+    --
+    -- PROGRAMMER: Jordan Lewis
+    --
+    -- DATE: March. 2nd, 2010
+    -------------------------------------------------------------------------*/
+    static std::map<int, Animation> getAnimationMap();
+private:
+   std::vector<Image> images_;
 
-	private:
-		std::string fileLocation_;
-		std::vector<Image> images_; /* stores vector of images */
-		int xOffSet_, yOffSet_;
-		int animationSpeed; /* will be used to control the speed of the animation based on type. */
-		AnimationType type; /* hold type of animation */
 };
 
-#endif
-
+#endif // ANIMATION_H
