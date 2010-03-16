ba885a2de069a75089d2125b8e50ea144bbf36a7
cdingo <cdingo@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-16 20:41:48 +0000

    makeUpdateObject complete

--- Server/ship.cpp
+++ Server/ship.cpp
@@ -3,3 +3,8 @@
 void Ship::applyActionMask(ActionMask newMask){
     actionMask = newMask;
 }
+UpdateObject Ship::makeUpdateObject(UpdateObject uo){
+    uo.pos_ = this.position;
+    uo.rotation_ = this.rotation;
+    uo.id = this.id;
+}
