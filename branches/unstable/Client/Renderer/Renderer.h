a84c30007e8658533f77ca6923fadb2251ab2c0a
kythyx <kythyx@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-20 00:49:38 +0000

    Renderer is in.

--- branches/unstable/Client/Renderer/Renderer.h
+++ branches/unstable/Client/Renderer/Renderer.h
@@ -1,27 +1,49 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
+#include <vector>
 #include <QGLWidget>
-#include <QGLFramebufferObject>
-//#include <QGLFramebufferObjectFormat>
+#include "../../Core/comm/data/updateobject.h"
+
+//PI macros
+#define PI              3.14159265358979323846
+#define TWOPI           (2.0 * PI)
+#define INVERSEPI       0.31830988618379067154
+
+//Degree macros
+#define DEGTORAD(a)     (a*PI/180.0)
+#define RADTODEG(a)     (a*180.0/PI)
+
+#define SCREENWIDTH     1024
+#define SCREENHEIGHT    768
+#define SCRCENTREW      512
+#define SCRCENTREH      384
+#define MAXRENDERCOUNT  40000
+
+struct RenderObject
+{
+    short x, y;
+    GLuint texture;
+    float texOffsetX, texOffsetY;
+    float objectWidth, objectHeight;
+    char rotation;
+    char objectWidthPx, objectHeightPx;
+};
 
 class Renderer : public QGLWidget
 {
+//vars
 private:
-    QPoint anchor;
-    float scale;
-    float rot_x, rot_y, rot_z;
-    GLuint tile_list;
-    GLfloat *wave;
+    RenderObject renderList[MAXRENDERCOUNT];
+    int renderCount;
+    GLuint textures[3];
 
-    QGLFramebufferObject *render_fbo;
-    QGLFramebufferObject *texture_fbo;
-
+//funcs
 public:
     Renderer(QWidget *parent);
     void Initialize();
     void Render();
-//    void paintEvent(QPaintEvent *);
+    void buildRenderList(std::vector<UpdateObject> objectlist);
 
 private:
     void resizeGL(int w, int h);
