a4ecf3fd25a104ef8e6d26cdc92b99b3afd02c5d
bradpaugh <bradpaugh@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-20 01:04:22 +0000

    directory structure

---
+++ Server/engine/ServerEngine.cpp
@@ -0,0 +1,92 @@
+#include "ServerEngine.h"
+
+using namespace std;
+
+/*------------------------------------------------------------------------------
+ --
+ -- CONSTRUCTOR: ServerEngine::ServerEngine()
+ --
+ -- DESIGNER: Brad Paugh
+ --
+ -- PROGRAMMER: Brad Paugh
+ --
+ -- REVISIONS:
+ --
+ -- NOTES:
+ -- This constructor initializes the Comm Server
+ --
+ -----------------------------------------------------------------------------*/
+ServerEngine::ServerEngine()
+{
+	server = CommServer::Instance();
+	server -> init();
+}
+
+/*------------------------------------------------------------------------------
+ --
+ -- CONSTRUCTOR: BaseWindow::BaseWindow()
+ --
+ -- DESIGNER: Brad Paugh
+ --
+ -- PROGRAMMER: Brad Paugh
+ --
+ -- REVISIONS:
+ --
+ -- NOTES:
+ -- This is the main loop for the server which handles all the
+ -- Messages and actions that come from clients.
+ --
+ -----------------------------------------------------------------------------*/
+void ServerEngine::RunServer()
+{
+	while(1)
+	{
+		/*todo: this is currently polling, very inefficient,
+			consult with the comm team to figure out
+			how this should be implemented.*/
+		if (server -> hasNextServerMessage())
+		{
+			ServerMessage sm = server->nextServerMessage();
+			
+			//user has logged in, add client id to ID vector
+			if (sm.GetMsgType() == ServerMessage::MT_LOGIN)
+			{
+				cout << "client logged in" << endl;
+				ids.push_back(sm.GetClientID());
+				ServerMessage init;
+				init.SetMsgType(ServerMessage::MT_INIT);
+				init.SetClientID(sm.GetClientID());
+				init.SetData("");
+				server->sendServerMsg(init);
+				/*check if we need to send the init message
+					or if its sent automatically on connect*/
+			}
+			//user has logged out, remove client id from vector
+			if (sm.GetMsgType() == ServerMessage::MT_CHAT)
+			{
+				size_t id = sm.GetClientID();
+				vector<int>::iterator it;
+				//iterate over all vectors and find the ID to delete
+				for (it = ids.begin(); it != ids.end(); it++)
+				{
+					if (id == *it)
+					{
+						ids.erase(it);
+					}
+				}
+			}
+			//receive chat message.. send to all clients
+			if (sm.GetMsgType() == 6)
+			{
+				server->sendServerMsg(sm, ids);
+			}
+		}
+		if (server -> hasNextClientAction())
+		{
+			ClientAction ca = server->nextClientAction();
+			//UpdateObject uo(ca.getClientID());
+			//gameplay updates updateObject here
+			//server -> sendUpdate(uo, ids);
+		}
+	}
+}
---
+++ Server/engine/main.cpp
@@ -0,0 +1,10 @@
+#include "ServerEngine.h"
+
+using namespace std;
+
+int main() 
+{
+	ServerEngine* se = new ServerEngine();
+	se->RunServer();
+	return 0;
+}
