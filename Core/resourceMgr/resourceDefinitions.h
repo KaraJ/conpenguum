38c2fb7acadd769b2afc07b0f9bd08e344cab4a3
jlewis604 <jlewis604@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-21 00:17:05 +0000

    altered resourceEnums to handle dynamic loading.

--- Core/resourceMgr/resourceDefinitions.h
+++ Core/resourceMgr/resourceDefinitions.h
@@ -1,9 +1,19 @@
 #ifndef RESOURCEDEFINITIONS_H
 #define RESOURCEDEFINITIONS_H
 #include <string>
+
+
 struct ResourceDefinition
 {
     int id;
+    std::string texture;
+    float texture_xoffset; // moved these up since they are saved in both JL.
+    float texture_yoffset;
+    float texture_width;
+    float texture_height;
+    int object_width;
+    unsigned int textureLocation;
+    int object_height;
 };
 
 struct ShipDefinition : public ResourceDefinition
@@ -13,24 +23,11 @@ struct ShipDefinition : public ResourceDefinition
     int velocity_max;
     int rotation_rate;
     int ship_hit_dist;
-    std::string texture;
-    float texture_xoffset;
-    float texture_yoffset;
-    float texture_width;
-    float texture_height;
-    int object_width;
-    int object_height;
+
 };
 
 struct ShotDefinition : public ResourceDefinition
 {
-    std::string texture;
-    float texture_xoffset;
-    float texture_yoffset;
-    float texture_width;
-    float texture_height;
-    int object_width;
-    int object_height;
 };
 
 #endif // RESOURCEDEFINITIONS_H
