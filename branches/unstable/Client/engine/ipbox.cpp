cc5433f3c55416c7bd77a53d6c34c6f50a912838
daraiusdastoor <daraiusdastoor@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-23 22:00:54 +0000

    rewrote the class

--- branches/unstable/Client/engine/ipbox.cpp
+++ branches/unstable/Client/engine/ipbox.cpp
@@ -1,15 +1,36 @@
 #include "ipbox.h"
 
-IpBox::IpBox(QWidget *parent) : QDialog(parent), ui(new Ui_IpBox)
+IpBox::IpBox(QWidget *parent, string alias, string addr, string prt) : QDialog(parent), ui(new Ui_IpBox), name(QString(alias.c_str())), ip(QString(addr.c_str())), port(QString(prt.c_str()))
 {
     ui->setupUi(this);
+
+    ui->nameentry->setText(name);
+    ui->ipentry->setText(ip);
+    ui->portentry->setText(port);
+
+    //show();
 }
 
 void IpBox::on_okbutton_clicked()
 {
-	ip = ui->ipentry->text().trimmed();
-	name = ui->nameentry->text().trimmed();
-	port = ui->portentry->text().trimmed();
+	ip = ui->ipentry->text();
+	name = ui->nameentry->text();
+	port = ui->portentry->text();
 
 	this->hide();
+}
+
+string IpBox::getName()
+{
+	return name.toStdString();
+}
+
+string IpBox::getIp()
+{
+	return ip.toStdString();
+}
+
+string IpBox::getPort()
+{
+	return port.toStdString();
 }
