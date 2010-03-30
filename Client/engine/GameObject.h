e7c162446d9ec135fde65fd01bcaaa87a7a34616
cryolithic <cryolithic@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-03-30 20:10:20 +0000

    adding stuff to automate updating

--- Client/engine/GameObject.h
+++ Client/engine/GameObject.h
@@ -5,19 +5,34 @@
 #include <qstring.h>
 #include "../Animation/Animation.h"
 #include "../Animation/Image.h"
+#include "../../Core/comm/data/updateobject.h"
 
 //0 ~ 65536
 
 struct GameObject
 {
-	int objectId;
-	int angle;
-        int animFrame;
-        QString text;
-	QPoint position;
-	Image * animeImage;
-        Animation currentAnime;
-	size_t animeIndex;
+    inline GameObject(){};
+    inline GameObject(UpdateObject &obj)
+    {
+        objectID = obj.getObjectId();
+        angle = obj.getRotation();
+        position = obj.getPos();
+    }
+    inline Update(UpdateObject &obj)
+    {
+        objectID = obj.getObjectId();
+        angle = obj.getRotation();
+        position = obj.getPos();
+    }
+
+    int objectId;
+    int angle;
+    int animFrame;
+    QString text;
+    QPoint position;
+    Image * animeImage;
+    Animation currentAnime;
+    size_t animeIndex;
 };
 
 #endif
