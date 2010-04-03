50bb0e0ed86cbc056973285dfc140800266eb204
daraiusdastoor <daraiusdastoor@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-04-03 00:17:37 +0000

    now reads sound links from xml and builds sound effect object (adds object to animation)

--- branches/unstable/Client/Animation/Animation.cpp
+++ branches/unstable/Client/Animation/Animation.cpp
@@ -34,7 +34,14 @@ map<int, Animation> Animation::getAnimationMap()
     animationExplosion = xmlParse->ReadAnimationVector(type, xmlFile); // loading all Explostion animations.
     imagesCount = animationExplosion.size();
     animationEx.setAnimationImages(animationExplosion);
-    animationEx.soundFile = animationExplosion.front().soundLink; // link sound from animation.
+    // set up sound effect for animation.
+	SoundEffects effectExplosion;
+	effectExplosion.effectPath_ = animationExplosion.front().soundLink;
+	effectExplosion.type_ = explosion;
+
+	//effectExplosion->SetSoundSource((char*)animationExplosion.front().soundLink.c_str(), explosion);
+	animationEx.setSoundEffect(effectExplosion);
+    //animationEx.soundFile = animationExplosion.front().soundLink; // link sound from animation.
     animations.insert(std::pair<int, Animation>((int)type, animationEx));
 
     // get ship animation
@@ -42,7 +49,7 @@ map<int, Animation> Animation::getAnimationMap()
 	animationShip = xmlParse->ReadAnimationVector(type, xmlFileSh); // loading all Explostion animations.
 	imagesCount = animationShip.size();
 	animationSh.setAnimationImages(animationShip);
-	animationSh.soundFile = animationShip.front().soundLink; // link sound from animation.
+	//animationSh.soundFile = animationShip.front().soundLink; // link sound from animation.
 	animationSh.numFrames_ = animationShip.front().numFrames; // link sound from animation.
 	animationSh.imagesTall_ = animationShip.front().imagesTall; // link sound from animation.
 	animationSh.imagesWide_ = animationShip.front().imagesWide; // link sound from animation.
@@ -53,7 +60,11 @@ map<int, Animation> Animation::getAnimationMap()
     animationExhaust = xmlParse->ReadAnimationVector(type, xmlFileExh);
     imagesCount = animationExhaust.size();
     animationExh.setAnimationImages(animationExhaust);
-    animationExh.soundFile = animationExhaust.front().soundLink; // link sound from animation.
+    SoundEffects effectExhaust;
+    effectExhaust.effectPath_ = animationExhaust.front().soundLink;
+    effectExhaust.type_ = exhaust;
+    animationExh.setSoundEffect(effectExhaust);
+    //animationExh.soundFile = animationExhaust.front().soundLink; // link sound from animation.
 	animations.insert(std::pair<int, Animation>((int)type, animationExh));
 
     mapSize = (int)animations.size();
