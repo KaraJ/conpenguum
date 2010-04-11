d6ddddb64e8f8227ef26c822616ad89515962ab8
bradpaugh <bradpaugh@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-04-11 00:38:22 +0000

    removed X Server dependancy for server

--- Server/engine/main.cpp
+++ Server/engine/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
-    QApplication app(argc, argv);
+    QApplication app(argc, argv, false);
 	ServerEngine *se = ServerEngine::GetInstance();
 
 	if (!se->Start())
