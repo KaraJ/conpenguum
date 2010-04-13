a04af133877188609dfd056fc61e4aaa9b1503d2
kythyx <kythyx@8e5970e0-fc99-11de-8589-c9554b7fe0d5>
2010-04-13 18:11:11 +0000

    rollem back

--- Server/comm/TCPServer.cpp
+++ Server/comm/TCPServer.cpp
@@ -1,17 +1,26 @@
 #include "TCPServer.h"
 
+
 using std::string;
 using std::queue;
 using std::map;
 using std::istringstream;
 using std::pair;
 
+int TCPServer::clientSockets_[MAX_CLIENTS];
+int TCPServer::maxClientSocket_ = 0;
+fd_set TCPServer::allSet_;
+sem_t *TCPServer::semSM_;
+queue<ServerMessage> *TCPServer::msgBuff_;
+map<int,in_addr> *TCPServer::clientAddressMap_;
+bool TCPServer::activeClients[MAX_CLIENTS];
+
 TCPServer::TCPServer()
 {
-	maxClientSocket_ = 0;
 	bzero(clientSockets_, MAX_CLIENTS * sizeof(int));
 	memset(activeClients, false, sizeof(bool) * MAX_CLIENTS);
 }
+
 
 void TCPServer::Init(const string port)
 {
@@ -34,53 +43,54 @@ void TCPServer::Init(const string port)
 
 void TCPServer::StartReadThread(queue<ServerMessage> *serverMsgs, map<int,in_addr> *clients, sem_t *semSM)
 {
-	msgBuff_ = serverMsgs;
-	semSM_ = semSM;
-	clientAddressMap_ = clients;
+	TCPServer::msgBuff_ = serverMsgs;
+	TCPServer::semSM_ = semSM;
+	TCPServer::clientAddressMap_ = clients;
 
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
-	if (pthread_create(&rThread_, &attr, TCPServer::ReadThread, this))
+	if (pthread_create(&rThread_, &attr, TCPServer::ReadThread, &listenSocket_))
 		Logger::LogNQuit("TCPServer: Unable to start listen thread.");
 }
 
 void* TCPServer::ReadThread(void* vptr)
 {
-	TCPServer* tcpServer = (TCPServer*) vptr;
+	int listenSocket = *(int*) vptr;
 	sockaddr_in sa;
 	int clientSocket, ready;
 	uint size;
 	fd_set currSet;
+	ServerMessage msgBuff;
 	bool isFull;
 
-	FD_ZERO(&tcpServer->allSet_);
-	FD_SET(tcpServer->listenSocket_, &tcpServer->allSet_);
-	SocketWrapper::Listen(tcpServer->listenSocket_, 5);
-	tcpServer->maxClientSocket_ = tcpServer->listenSocket_;
+	FD_ZERO(&allSet_);
+	FD_SET(listenSocket, &allSet_);
+	SocketWrapper::Listen(listenSocket, 5);
+	maxClientSocket_ = listenSocket;
 
 	for (int i = 0; i < MAX_CLIENTS; i++)
-		tcpServer->clientSockets_[i] = 0;
+		clientSockets_[i] = 0;
 
 	while (true)
 	{
-		currSet = tcpServer->allSet_;
+		currSet = allSet_;
 
-		ready = select(tcpServer->maxClientSocket_ + 1, &currSet, NULL, NULL, NULL);
+		ready = select(maxClientSocket_ + 1, &currSet, NULL, NULL, NULL);
 
-		if (FD_ISSET(tcpServer->listenSocket_, &currSet))
+		if (FD_ISSET(listenSocket, &currSet))
 		{
 			size = sizeof(sa);
-			clientSocket = SocketWrapper::Accept(tcpServer->listenSocket_, &sa, &size);
+			clientSocket = SocketWrapper::Accept(listenSocket, &sa, &size);
 			isFull = true;
 			for (int i = 0; i < MAX_CLIENTS; ++i)
 			{
-				if (tcpServer->clientSockets_[i] == 0)
+				if (clientSockets_[i] == 0)
 				{
-					tcpServer->clientAddressMap_->insert(pair<int, in_addr>(i,sa.sin_addr));
-					tcpServer->clientSockets_[i] = clientSocket;
-					if (clientSocket > tcpServer->maxClientSocket_)
-						tcpServer->maxClientSocket_ = clientSocket;
-					FD_SET(clientSocket, &tcpServer->allSet_);
+					clientAddressMap_->insert(pair<int, in_addr>(i,sa.sin_addr));
+					clientSockets_[i] = clientSocket;
+					if (clientSocket > maxClientSocket_)
+						maxClientSocket_ = clientSocket;
+					FD_SET(clientSocket, &allSet_);
 					isFull = false;
 					break;
 				}
@@ -98,28 +108,27 @@ void* TCPServer::ReadThread(void* vptr)
 		}
 		for (int clientID = 0; clientID < MAX_CLIENTS; ++clientID)
 		{
-			if (tcpServer->clientSockets_[clientID] == 0)
+			if ((clientSocket = clientSockets_[clientID]) == 0)
 				continue;
 
 			if (FD_ISSET(clientSocket, &currSet))
 			{
-				ServerMessage incomingMessage;
-				if (TCPConnection::ReadMessage(tcpServer->clientSockets_[clientID], incomingMessage))
+				if (TCPConnection::ReadMessage(clientSocket, msgBuff))
 				{
-					switch (incomingMessage.GetMsgType())
+					switch (msgBuff.GetMsgType())
 					{
 					case ServerMessage::MT_LOGIN: //If login msg, client doesnt know own id yet - add it
-						incomingMessage.SetClientID(clientID);
+						msgBuff.SetClientID(clientID);
 						break;
 					}
-					sem_wait(tcpServer->semSM_);
-					tcpServer->msgBuff_->push(incomingMessage);
-					sem_post(tcpServer->semSM_);
+					sem_wait(semSM_);
+					msgBuff_->push(msgBuff);
+					sem_post(semSM_);
 				}
 				else
 				{
-					tcpServer->SendLogoutMessage(clientID);
-					tcpServer->ClientDC(clientID);
+					SendLogoutMessage(clientID);
+					ClientDC(clientID);
 				}
 			}
 			if (--ready == 0)
